ef92f3d58f8a6b9fadc8978e28d33abe7071128e
Maria Luiza <co.maria.luiza@gmail.com>
2020-06-12 09:24:36 -0300

    Removed personal links

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLinkedin, FiGithub, FiMail, FiPhone } from 'react-icons/fi';
+import { FiLinkedin, FiGithub } from 'react-icons/fi';
 
 import { Container } from './styles';
 
@@ -15,15 +15,9 @@ export default function About() {
 
                 <p>Eu desenvolvo aplicações web que focam na experiência do usuário utilizando as melhores tecnologias presentes no mercado. Sou muito organizada e procuro um aprendizado constante, sempre estudando para aumentar meu conhecimento.</p>
 
-                <p>Eu estou disponível para trabalhos freelance! Sinta-se livre pra entrar em contato comigo e me adicionar no LinkedIn.</p>
-
                 <div className="links">
                     <a href="https://www.linkedin.com/in/marialuizacoelho/" target="_blank" rel="noopener noreferrer"><FiLinkedin />Linkedin</a>
                     <a href="https://github.com/comarialuiza" target="_blank" rel="noopener noreferrer"><FiGithub /> Github</a>
-                </div>
-                <div className="links">
-                    <a href="https://api.whatsapp.com/send?phone=5541991212363" target="_blank" rel="noopener noreferrer"><FiPhone /> (41) 9 9121 2363</a>
-                    <a href="mailto:co.maria.luiza@gmail.com" target="_blank" rel="noopener noreferrer"><FiMail /> co.maria.luiza@gmail.com</a>
                 </div>
             </div>
         </Container>
